7a20ccb103dff556c43723a7a8a12afe2d64178b
vitalik-ez <vetalyeshor@gmail.com>
2021-09-14 20:21:25 +0300

    deploy

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/vitalik-ez/ElifTech-Bank-Golang/pkg/handler"
 	"github.com/vitalik-ez/ElifTech-Bank-Golang/pkg/repository"
 	"github.com/vitalik-ez/ElifTech-Bank-Golang/pkg/service"
@@ -10,13 +12,23 @@ import (
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
 
-	if err := srv.Run("3000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(port, handlers.InitRoutes()); err != nil {
 		log.Fatal("Error occured while running http server", err.Error())
 	}
 
