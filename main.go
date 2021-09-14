130b715dc2281e4bdaca397660e3b6f47b1e1282
vitalik-ez <vetalyeshor@gmail.com>
2021-09-14 20:04:01 +0300

    deploy

--- main.go
+++ main.go
@@ -10,20 +10,13 @@ import (
 )
 
 func main() {
-	/*
-		err := godotenv.Load()
-		if err != nil {
-			log.Println("Error loading .env file")
-		}
-		port := os.Getenv("PORT")*/
-
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
 
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run("3000", handlers.InitRoutes()); err != nil {
 		log.Fatal("Error occured while running http server", err.Error())
 	}
 
