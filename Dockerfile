7b53494885e73dd0130e9d6b3772714c5fbc6d95
vitalik-ez <vetalyeshor@gmail.com>
2021-09-15 01:59:33 +0300

    debug

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM golang:1.16.7
-RUN mkdir /app
-ADD . /app
-WORKDIR /app
-RUN go mod download
-RUN go build -o main ./cmd/bank-service/
-CMD ["/app/main"]
--- schema/000001_init.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS banks (
-    id serial not null UNIQUE,
-    name varchar(255) not null,
-    interestRate  real not null,
-    maximumLoan  real not null,
-    minimumDownPayment  real not null,
-    loanTermInMonths  integer not null,
-);
